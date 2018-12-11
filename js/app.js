2e1d15d9d237c81c1b527ad9858646dfce558a79
Daniella <delgadodaniellan@gmail.com>
2018-12-11 13:56:00 -0500

    Removed calendar Js
    Removed the JavaScript in Calendar, caused assignments page to disappear

--- js/app.js
+++ js/app.js
@@ -171,23 +171,6 @@ document.getElementById('page').addEventListener('page', function (e) {
 
 
 //CALENDAR
-var duedates = [
-  {task: "", due: {y:2018, m:10, d:4} },
-  {task: "", due: {y:2018, m:10, d:16} },
-  {task: "", due: {y:2018, m:10, d:21} }
-];
-for (var date in duedates) {
-  var due = duedates[date].due;
-  console.log(`.calendar li[data-date="${due.y}-${due.m}-${due.d}"]`);
-  var caldate = document.querySelector(`.calendar li[data-date="${due.y}-${due.m}-${due.d}"]`);
-  caldate.classList.add('due');
-  caldate.innerHTML += ('<br>' + duedates[date].task);
-}
-
-
-var d = new Date();
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-document.getElementById("demo1").innerHTML = months[d.getMonth()];
 
 
 // FAKE DATA
